53e9c437b0d3fcbf819ab98a4832a0c74daf4092
Kavyagupta070707 <kavyagupta0105@gmail.com>
2026-01-08 23:30:22 +0530

    home and problem page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes , Route, Navigate} from 'react-router'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import { Toaster } from 'react-hot-toast';
+import ProblemsPage from './pages/ProblemsPage.jsx';
 function App() {
   
   const {isSignedIn}= useUser();
@@ -14,7 +15,8 @@ function App() {
       
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/problem" element={isSignedIn?<Problem/>: <Navigate to="/" />}/>
+        <Route path="/problems" element={<ProblemsPage/>}/>
+        {/* <Route path="/problems" element={isSignedIn?<ProblemsPage/>: <Navigate to="/" />}/> */}
     
       </Routes>
       <Toaster/>
