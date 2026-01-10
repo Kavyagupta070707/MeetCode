c653b03c42bbe37c8f6c8ad11b6770e2b3cbc77c
Kavyagupta070707 <kavyagupta0105@gmail.com>
2026-01-10 18:43:16 +0530

    fixes

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -6,7 +6,7 @@ export const ENV = {
     MONGODB_URI : process.env.MONGODB_URI,
     INNGEST_EVENT_KEY : process.env.INNGEST_EVENT_KEY,
     INNGEST_SIGNING_KEY : process.env.INNGEST_SIGNING_KEY,
-    SREAM_API_KEY : process.env.SREAM_API_KEY,
+    STREAM_API_KEY : process.env.STREAM_API_KEY,
     STREAM_API_SECRET : process.env.STREAM_API_SECRET,
     NODE_ENV : process.env.NODE_ENV,
     
